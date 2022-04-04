46a67b691f54b6c3e0238dfb4f8bab84bfa0bd44
Jacob Gad <jacob.gad@hotmail.com>
2022-04-04 10:18:09 +1000

    add dark theme and theme toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,49 @@
-import { Box, Tabs, Tab } from '@mui/material';
+import { Brightness2, Brightness7 } from '@mui/icons-material';
+import { Box, Tabs, Tab, ThemeProvider, createTheme, IconButton, CssBaseline } from '@mui/material';
 import { useState } from 'react';
 import Acknowledgements from './Acknowledgements';
 import Contacts from './Contacts';
 import Requirements from './Requirements';
+import useLocalStorage from './utils/useLocalStorage';
 
 export default function App() {
 	const [value, setValue] = useState(1);
+	const [darkTheme, setDarkTheme] = useLocalStorage('darkTheme', false);
+
+	const theme = createTheme({
+		palette: {
+			mode: darkTheme ? 'dark' : 'light',
+		},
+	});
 
 	const handleChange = (event: React.SyntheticEvent, newValue: number) => {
 		setValue(newValue);
 	};
 
 	return (
-		<>
-			<Box sx={{ borderBottom: 1, borderColor: 'divider', mb: 6 }}>
-				<Tabs value={value} onChange={handleChange} aria-label='basic tabs example'>
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<Box
+				sx={{
+					mb: 6,
+					borderBottom: 1,
+					borderColor: 'divider',
+					display: 'flex',
+					justifyContent: 'space-between',
+				}}
+			>
+				<Tabs value={value} onChange={handleChange}>
 					<Tab label='Requirements' />
 					<Tab label='Contacts App' />
 					<Tab label='Acknowledgements' />
 				</Tabs>
+				<IconButton sx={{ mr: 3 }} onClick={() => setDarkTheme(!darkTheme)} color='inherit'>
+					{darkTheme ? <Brightness2 /> : <Brightness7 />}
+				</IconButton>
 			</Box>
 			{value === 0 && <Requirements />}
 			{value === 1 && <Contacts />}
 			{value === 2 && <Acknowledgements />}
-		</>
+		</ThemeProvider>
 	);
 }
